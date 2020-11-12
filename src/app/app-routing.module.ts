87211da868ebd54da8479798f8275803b094f9cf
Agustin Dllano <agus.dllano@gmail.com>
2020-11-12 18:36:03 -0300

    agregados componentes carrito y about. Agregado router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { EmpanadasListComponent } from './empanadas-list/empanadas-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'empanadas', pathMatch: 'full' },
+  { path: 'empanadas', component: EmpanadasListComponent },
+  { path: 'about', component: AboutComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmpanadasListComponent } from './empanadas-list/empanadas-list.component';
+import { AboutComponent } from './about/about.component';
+import { CartManagerService } from './cart-manager.service';
+import { CarritoComponent } from './empanadas-list/carrito/carrito.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmpanadasListComponent
+    EmpanadasListComponent,
+    AboutComponent,
+    CarritoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CartManagerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
